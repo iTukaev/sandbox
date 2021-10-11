720bad78a526dc65ac1b1b530007159ff66d1e34
iTukaev <2234944@gmail.com>
2021-10-11 22:44:10 +0500

    programm created, txt files deleted from commit

--- 26.go
+++ 26.go
@@ -3,22 +3,69 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
+func readFile(name string) string {
+	file, err := os.Open(name)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(b)
+}
+
 func main () {
-	var flag1, flag2, flag3 string
+	var flags = []string{"", "", ""}
 
-	//flag.StringVar(&flag1, "", "","")
-	//flag.StringVar(&flag2, "", "","")
-	//flag.StringVar(&flag3, "", "","")
-
-	//flag.Parse()
-	flag.StringVar(&flag1, "first", "", "first file name")
-	flag.StringVar(&flag2, "second", "", "second file name")
-	flag.StringVar(&flag3, "result", "", "result file name")
-
+	flag.StringVar(&flags[0], "first", "", "first file name")
+	flag.StringVar(&flags[1], "second", "", "second file name")
+	flag.StringVar(&flags[2], "result", "", "result file name")
 	flag.Parse()
-	fmt.Printf("%v\t%v\t%v\t", flag1, flag2, flag3)
+
+	var  checkFiles []bool
+	var result []string
+
+	for i := range flags {
+		checkFiles = append(checkFiles, flags[i] != "")
+	}
+
+	if checkFiles[0] {
+		result = append(result, readFile(flags[0]))
+	}
+
+	if checkFiles[1] {
+		result = append(result, readFile(flags[1]))
+	}
+
+	resultString := strings.Join(result, "\n")
+
+	if !checkFiles[2] {
+		fmt.Println("Результат чтения:")
+		fmt.Println(resultString)
+	} else {
+		file, err := os.Create(flags[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		b := bytes.NewBufferString(resultString)
+
+		if err := ioutil.WriteFile(flags[2], b.Bytes(), 0777); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
